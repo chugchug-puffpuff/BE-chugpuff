c321a90040257e16a7833a2644d4a094624162ec
pia01190 <piakim0119@gmail.com>
2024-06-29 16:02:12 +0900

    init: 스프링 프로젝트 생성

---
+++ src/main/java/chugpuff/chugpuff/ChugpuffApplication.java
@@ -0,0 +1,13 @@
+package chugpuff.chugpuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChugpuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChugpuffApplication.class, args);
+	}
+
+}
