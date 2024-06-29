f86a83dddd224e5f424ea498a48a8d25239db2a6
pia01190 <piakim0119@gmail.com>
2024-06-29 21:53:58 +0900

    #1 feat: Member 도메인 구현

--- src/main/java/chugpuff/chugpuff/domain/Member.java
+++ src/main/java/chugpuff/chugpuff/domain/Member.java
@@ -1,4 +1,41 @@
 package chugpuff.chugpuff.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "members")
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDate birth;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String job;
+
+    @Column(nullable = false)
+    private String jobKeyword;
+
 }
