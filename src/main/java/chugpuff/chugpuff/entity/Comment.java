2c018357c5159dc751d4592843e20eea94a8feb7
dpwl0974 <67728547+dpwl0974@users.noreply.github.com>
2024-07-02 13:38:32 +0900

    #5 Feat: use_id 외래키 사용 추가

--- src/main/java/chugpuff/chugpuff/entity/Comment.java
+++ src/main/java/chugpuff/chugpuff/entity/Comment.java
@@ -18,7 +18,9 @@ public class Comment {
     @JoinColumn(name = "board_no")
     private Board board; // 게시글 번호 외래키로 매핑
 
-    private String userId; //유저 ID (댓글 작성자)
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user; // 유저와의 관계, 외래키로 user_id 사용
 
     @Column(columnDefinition = "TEXT")
     private String bcContent; //댓글 내용
