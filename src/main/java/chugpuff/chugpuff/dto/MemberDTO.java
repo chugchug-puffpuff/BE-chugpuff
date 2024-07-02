67d9fb273cf2d9fd5c4266bc9e4ff43aed5154f6
pia01190 <piakim0119@gmail.com>
2024-07-02 14:59:58 +0900

    #3 toEntity 메서드 추가

--- src/main/java/chugpuff/chugpuff/dto/MemberDTO.java
+++ src/main/java/chugpuff/chugpuff/dto/MemberDTO.java
@@ -24,4 +24,20 @@ public class MemberDTO {
     private Boolean termsAccepted;
     private Boolean privacyPolicyAccepted;
     private Boolean recordingAccepted;
+
+    public Member toEntity() {
+        Member member = new Member();
+        member.setUser_id(this.user_id);
+        member.setPassword(this.password);
+        member.setName(this.name);
+        member.setBirth(this.birth);
+        member.setEmail(this.email);
+        member.setJob(this.job);
+        member.setJobKeyword(this.jobKeyword);
+        member.setIsAbove15(this.isAbove15);
+        member.setTermsAccepted(this.termsAccepted);
+        member.setPrivacyPolicyAccepted(this.privacyPolicyAccepted);
+        member.setRecordingAccepted(this.recordingAccepted);
+        return member;
+    }
 }
