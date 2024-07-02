d0d5023f7133db237e89fea951908bfba71c93a2
dpwl0974 <67728547+dpwl0974@users.noreply.github.com>
2024-07-02 14:25:12 +0900

    Commnet:코드 설명 주석 추가

--- src/main/java/chugpuff/chugpuff/repository/CategoryRepository.java
+++ src/main/java/chugpuff/chugpuff/repository/CategoryRepository.java
@@ -4,5 +4,6 @@ import chugpuff.chugpuff.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    //카테고리 이름으로 카테고리 조회
     Category findByCategoryName(String categoryName);
 }
