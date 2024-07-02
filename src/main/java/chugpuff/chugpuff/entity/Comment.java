cbaca68d769c0ab1696e99bb7485c05d814d6c9a
dpwl0974 <67728547+dpwl0974@users.noreply.github.com>
2024-07-02 13:28:27 +0900

    #5 Comment: Comment entity 주석 추가 및 컬럼 타입 지정

--- src/main/java/chugpuff/chugpuff/entity/Comment.java
+++ src/main/java/chugpuff/chugpuff/entity/Comment.java
@@ -12,14 +12,17 @@ import java.time.LocalDateTime;
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int bcNo; //댓글 번호
+    private int bcNo; //댓글 번호, 자동 생성되는 기본키
 
     @ManyToOne
     @JoinColumn(name = "board_no")
-    private Board board;
+    private Board board; // 게시글 번호 외래키로 매핑
 
-    private String userId; //유저 ID
+    private String userId; //유저 ID (댓글 작성자)
+
+    @Column(columnDefinition = "TEXT")
     private String bcContent; //댓글 내용
+
     private LocalDateTime bcDate; //댓글 작성일
     private LocalDateTime bcmodifiedDate; //댓글 수정일
 }
