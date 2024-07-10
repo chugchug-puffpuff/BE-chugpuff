1d6ff9ca7e3acf6d408b20197249ee7cb54f644a
pia01190 <piakim0119@gmail.com>
2024-07-10 22:29:12 +0900

    #12 feat: SecurityConfig 로그인 접근 권한 수정

--- src/main/java/chugpuff/chugpuff/security/SecurityConfig.java
+++ src/main/java/chugpuff/chugpuff/security/SecurityConfig.java
@@ -15,30 +15,26 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/login", "/members").permitAll()
-                                .anyRequest().authenticated()
-                )
+                .authorizeHttpRequests(authorizeRequests -> {
+                    authorizeRequests
+                            .requestMatchers("/login", "/members").permitAll()
+                            .anyRequest().authenticated();
+                })
                 .formLogin(formLogin ->
                         formLogin
-                                // 로그인 페이지 설정
-                                .loginPage("/login")
                                 .defaultSuccessUrl("/home", true)
                                 .permitAll()
                 )
                 .logout(logout ->
                         logout
-                                // 로그아웃 설정
                                 .logoutUrl("/logout")
                                 .logoutSuccessUrl("/login?logout")
                                 .permitAll()
                 )
                 .rememberMe(rememberMe ->
                         rememberMe
-                                // 로그인 유지 설정
                                 .key("uniqueAndSecret")
-                                .tokenValiditySeconds(604800) // 7일 동안 유지
+                                .tokenValiditySeconds(604800)
                 );
 
         return http.build();
