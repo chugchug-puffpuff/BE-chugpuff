502267103bc803109a77ba4d8557ec9b385d2132
pia01190 <piakim0119@gmail.com>
2024-07-10 21:19:28 +0900

    #12 feat: SecurityConfig 구현

---
+++ src/main/java/chugpuff/chugpuff/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package chugpuff.chugpuff.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/login", "/members").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                // 로그인 페이지 설정
+                                .loginPage("/login")
+                                .defaultSuccessUrl("/home", true)
+                                .permitAll()
+                )
+                .logout(logout ->
+                        logout
+                                // 로그아웃 설정
+                                .logoutUrl("/logout")
+                                .logoutSuccessUrl("/login?logout")
+                                .permitAll()
+                )
+                .rememberMe(rememberMe ->
+                        rememberMe
+                                // 로그인 유지 설정
+                                .key("uniqueAndSecret")
+                                .tokenValiditySeconds(604800) // 7일 동안 유지
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
