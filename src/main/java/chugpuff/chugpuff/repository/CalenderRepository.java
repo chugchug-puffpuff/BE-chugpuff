87d29e996532aac7837b72c8fd65b7ef64a3949f
dpwl0974 <67728547+dpwl0974@users.noreply.github.com>
2024-09-03 21:13:28 +0900

    #32 스크랩한 공고 마감기한 알림 제공 기능 추가

--- src/main/java/chugpuff/chugpuff/repository/CalenderRepository.java
+++ src/main/java/chugpuff/chugpuff/repository/CalenderRepository.java
@@ -5,11 +5,15 @@ import chugpuff.chugpuff.entity.Calender;
 import chugpuff.chugpuff.entity.Scrap;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface CalenderRepository extends JpaRepository<Calender, Long> {
     List<Calender> findByMember(Member member);
 
-    // Scrap을 기준으로 캘린더 항목을 찾는 메서드 추가
+    // Scrap을 기준으로 캘린더 항목 찾기
     List<Calender> findByScrap(Scrap scrap);
+
+    //마감기한 D-1 스크랩 공고 조회
+    List<Calender> findByMemberAndMemoDate(Member member, String memoDate);
 }
